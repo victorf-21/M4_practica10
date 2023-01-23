cc7a2b10e3a863d185ce4fad9a301ec2ff09ed92
albertpenades3 <apenad22@jaumebalmes.net>
2023-01-23 20:51:47 +0100

    Subido por albert

---
+++ B/exercici2_B.py
@@ -0,0 +1,12 @@
+myTupla = (0,3,0,1,0,3)
+print("La longitud de la tupla és de: ")
+print(len(myTupla))
+# Indica la longitud de la tupla
+
+print("El número 0 es repeteix: ")
+print(myTupla.count(0))
+# # Indica el valor 0 quantes vegades es repeteix.
+
+print("El valor 3 es repeteix: ")
+print(myTupla.count(3))
+# Indica el valor 3 quantes vegades es repeteix.
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+
+# Victor (A)
 def fecha(a,b):
+    return a-b
+
+
+# Albert (B)
+def restar(a,b):
     return a-b
