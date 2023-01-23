7e8c36bf2b5ce05c3b33047814a27c1ba609a4dc
victorf-21 <vfernand22@jaumebalmes.net>
2023-01-23 20:40:30 +0100

    Subido por Victor

---
+++ A/exercici1_A.py
@@ -0,0 +1,4 @@
+#Llamar la función del archivo
+import main as edad
+#Restar el año actual con el año de mi nacimiento
+print(edad.fecha(2023,2004))
---
+++ A/exercici2_A.py
@@ -0,0 +1,8 @@
+myDict = {'Instituto': 'Jaume Balmes', 'Alumno': 'Victor', 'Edad': 19, 'Clase':'DAW', 'Piso': 2, 'Año' : 2023 }
+#La longitud del diccionari
+print(len(myDict))
+#Els valors
+print(myDict.values())
+#Extreure l'ultim item
+print(myDict.popitem())
+
